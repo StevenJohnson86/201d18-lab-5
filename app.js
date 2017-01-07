3775e2d3ae421a5d13f6a888b4ab7ddbae094e14
Steven Johnson <stevenjohnson86@gmail.com>
2017-01-06 17:01:09 -0800

    sumAndMultiply function complete

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a,b){ //eslint-disable-line
   var result = a + b;
-  var message = 'The sum of ' + a + ' and ' + b
- + ' is ' + result + '.';
+  var message = 'The sum of ' + a + ' and ' + b + ' is ' + result + '.';
   return [result, message];
 }
 
@@ -53,11 +52,22 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+  console.log('a,b,c', a, b, c);
+  var sum1 = sum(a, b);
+  console.log('sum1 = ', sum1);
+  var sum2 = sum(sum1[0], c);
+  console.log('sum2 = ', sum2);
+  var product1 = multiply(a, b);
+  var product2 = multiply(product1[0], c);
+  var messageSum = a + ' and ' + b + ' and ' + c + ' sum to ' + sum2[0] + '.';
+  var messageProduct = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + product2[0] + '.';
+//  console.log(messageSum);
+//  console.log(messageProduct);
+  return [sum2[0], product2[0], messageSum, messageProduct];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
